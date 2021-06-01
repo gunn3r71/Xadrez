e0fcb08f5491394d75d2bdaa272f795bc741bf6f
gunn3r71 <lucasgunner3@gmail.com>
2021-05-31 21:49:55 -0300

    Classe posição xadrez

---
+++ Xadrez/Jogo/PosicaoXadrez.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xadrez.Tabuleiro;
+
+namespace Xadrez.Jogo
+{
+    class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.Coluna = coluna;
+            this.Linha = linha;
+        }
+
+        public Posicao ConverterPosicao()
+        {
+            return new Posicao(8 - this.Linha, this.Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Coluna}{this.Linha}"; 
+        }
+    }
+}
