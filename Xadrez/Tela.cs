27d59ed1e3d90e604c9ce53f380532315183ab1c
gunn3r71 <lucasgunner3@gmail.com>
2021-05-31 22:14:35 -0300

    Melhorando impressão do tabuleiro

--- Xadrez/Tela.cs
+++ Xadrez/Tela.cs
@@ -7,13 +7,37 @@ namespace Xadrez
     {
         public static void ImprimirTabuleiro(TabuleiroXadrez tabuleiro)
         {
+            int marcacaoTabuleiro = 8;
             for (int i = 0; i < tabuleiro.Linhas; i++)
             {
+                Console.Write($"{marcacaoTabuleiro--} ");
                 for (int j = 0; j < tabuleiro.Colunas; j++)
                 {
-                    Console.Write((tabuleiro.GetPeca(new Posicao(i,j)) != null) ? $"{tabuleiro.GetPeca(new Posicao(i, j))} " : $"- "); ;
+                    Peca peca = tabuleiro.GetPeca(new Posicao(i, j));
+                    if (peca != null)
+                    {
+                        ImprimirPeca(peca);
+                    } else
+                    {
+                        Console.Write(" -");
+                    }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("   a b c d e f g h");
+        }
+
+        public static void ImprimirPeca(Peca peca)
+        {
+            if (peca.Cor == Cor.Branca)
+            {
+                Console.Write($" {peca}");
+            } else
+            {
+                var aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write($" {peca}");
+                Console.ForegroundColor = aux;
             }
         }
     }
